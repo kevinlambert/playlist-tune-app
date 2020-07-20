89e179caa657a185ae896f29f97f70173e5b5926
Kevin Lambert <caylam@Kevins-MBP.lan>
2020-07-20 14:04:37 +0100

    flex-end

--- components/socialShare.js
+++ components/socialShare.js
@@ -2,7 +2,7 @@ import { Facebook, Twitter } from "react-social-sharing";
 
 export default () => {
   return (
-    <div style={{ "align-self": "end" }}>
+    <div style={{ "align-self": "flex-end" }}>
       <Facebook solid small link="https://playlisttune.com" />
       <Twitter solid small link="https://playlisttune.com" />
     </div>
